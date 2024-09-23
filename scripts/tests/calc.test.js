978f256fd5b10ba5abc7877b2875b04fc4cb997f
Maeve Crossan <maevecrossan@gmail.com>
2024-09-23 17:08:01 +0000

    Update addition function

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const { TestScheduler } = require("jest");
 const addition = require("../calc");
 
@@ -5,6 +9,9 @@ describe("Calculator", () => {
     describe("Addition function", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20,22)).toBe(42);
+        });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42,31)).toBe(73);
         });
     });
     describe("Subtraction function", () => {
